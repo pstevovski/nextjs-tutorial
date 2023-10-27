811080db9763b101d075043e664c08ab17e1ce9d
Petar Stevovski <petar@codeart.mk>
2023-10-27 15:11:46 +0200

    Chapters #1 to #6 done

---
+++ app/ui/fonts.ts
@@ -0,0 +1,7 @@
+import { Inter, Lusitana } from "next/font/google";
+
+export const FontInter = Inter({ subsets: ["latin"] });
+export const FontLusitana = Lusitana({
+  subsets: ["latin"],
+  weight: ["400", "700"],
+});
